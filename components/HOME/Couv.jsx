4ac872d4a3ab5542818af932761389e170b23d58
yahia <yahia.slimani@univ-constantine2.dz>
2024-09-03 16:19:51 +0100

    responsive couv

--- components/HOME/Couv.jsx
+++ components/HOME/Couv.jsx
@@ -1,24 +1,56 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Image from 'next/image'
+import { Check, CloudeCheck, Mail } from '../../svg'
 export default function Couv() {
   return (
-    <section className="flex mt-5" >
-        <div className="flex flex-col justify-center items-center gap-3 w-1/2">
-            <h1 className="font-bold text-5xl mb-0">Un apprentissage illimité à portée
+    <section className="couv flex mt-5 flex-col lg:flex-row gap-14 lg:gap-4" >
+        <div className="flex flex-col justify-center items-center gap-3 w-full lg:w-1/2">
+            <h1 className="font-bold text-3xl sm:text-5xl mb-0">Un apprentissage illimité à portée
             <span className='relative'>
              <span className='z-20 relative ps-3 '>De main</span>
              <span className='absolute z-10 top-0 left-0'>
-                <svg width="300px"  height="62.1px" enableBackground="new 0 0 366 62.1" viewBox="0 0 366 62.1" xmlns="http://www.w3.org/2000/svg">
+                <svg className='hidden xl:block' width="300px"  height="62.1px" enableBackground="new 0 0 366 62.1" viewBox="0 0 366 62.1" xmlns="http://www.w3.org/2000/svg">
                     <path className="fill-yellow-500" d="m322.5 25.3c0 1.4 2.9 0.8 3.1 1.6 0.8 1.1-1.1 1.3-0.6 2.4 13.3 0.9 26.9 1.7 40.2 4-2.5 0.7-4.9 1.6-7.3 1.1-4-0.9-8.2-1-12.2-1.2-8.5-0.5-16.9-1.9-25.5-1.7h-3.1c2.6 0.6 4.8 0.4 5.7 2.2-7.3 0.4-14.1-0.8-21.2-1.1-0.2 0.6-0.5 1.2-0.8 1.8 21.3 0.7 42.5 1.6 64.3 4.6-4.2 1.6-7.7 1-10.8 0.8-6.8-0.5-13.5-1.3-20.3-1.9-0.9-0.1-2.3-0.1-2.9 0.2-2.2 1.6-4.3 0.6-7 0.4 1.4-1 2.5 0.5 3.9-0.8-5.6-1-10.7 0.6-15.9 0s-10.5-0.6-16.6-0.8c2 1.6 4.6 1.3 6.2 1.4 4.9 0 9.9 0.8 14.8 0.7 5.3-0.1 10.4 0.5 15.5 0.9 3.2 0.3 6.7-0.1 9.9-0.4 1.1-0.1 0.5 0.3 0.6 0.6 0.5 0.9 2.2 0.8 3.6 0.8 5.1-0.1 10.1 0.6 14.8 1.5 0.8 0.1 1.5 0 1.7 0.7 0 0.7-0.8 0.6-1.5 0.8-3.9 1.2-7.4-0.2-11.1-0.2-2 0-4.3-1.5-6 0.5-0.3 0.4-1.4 0.1-2.2-0.1-4.5-0.8-9.1-0.5-13.8-1.5-2.3-0.5-5.6 0.1-8.4 0.5-4 0.5-8-0.7-12.1-0.9-3.4-0.2-7.1-0.5-10.5-0.7-7.1-0.3-14.2-1.2-22.3-0.4 4.9 1.1 9.4 1.2 13.8 1.2 9.7 0 19.2 2.3 28.9 1.6 7.3 3.2 15.9 1.5 23.8 2.9 4.9 0.8 10.1 0.8 15.2 1.2 0.5 0 0.8 0.3 1.1 0.9-20-2.1-40.2-1.4-60.8-3 4.9 2.1 10.8-0.3 15.3 2.7-8 1.9-15.8-0.9-23.5-0.1 2.8 1.4 7.1 1.1 9.3 3.3 0.5 0.5 0.2 1.1-1.2 1.3 2.3 1 3.4-2.1 5.7-0.4 0.2-0.6 0.2-1 0.3-1.5 0.8-0.3 2 0.8 1.5 1.5-0.2 0.1 0 0.3 0 0.5 18.7 0.4 37.3 1.7 56.2 3.6-1.7 1.1-2.8 1.2-4.2 1.1-7.1-0.5-14.1-0.9-21.2-1.4-3.1-0.2-6.3-0.4-9.4-0.4-7.6-0.2-15-0.7-22.4-1-9-0.4-17.9-0.1-26.9-0.1-1.2 0-2.9-0.4-3.9 1 14.8 0.3 29.7 0.6 44.4 1.1 14.8 0.6 29.9 1.3 44.2 4.2-4.3 1-8.8 0.9-13 0.5-5.3-0.5-10.5-1.1-15.8-1.2-11.4-0.3-22.9-0.9-34.3-1.2-17.6-0.4-35.4-0.3-53.1-0.4-10.8-0.1-21.7-0.2-32.5 0-17.8 0.4-35.7 0.2-53.5 0.5-13.1 0.3-26.3 0.1-39.4 0.5-11.1 0.3-22.4 0.6-33.6 1-13.1 0.6-26.1 0.2-39.3 0.4-3.9 0.1-7.6 0.2-11.8-0.2 0.9-1.2 2.3-1.3 3.9-1.3 8.4 0.2 16.6-0.4 24.9-0.9 3.9-0.2 7.9-0.4 11.9 0.2 2.5 0.4 5.3-0.3 8-0.4 7.3-0.4 14.7-0.7 22-0.9 11.9-0.5 23.7-1.2 35.6-0.8 7.7 0.2 15.3-0.6 22.9-0.1 2.3 0.2 4.3-0.5 6.5-1h-17.6c-9.6 0-19-0.1-28.6 0-8 0.1-16.1 0.3-24 0.8-2.6 0.2-5.4 0.1-8.2 0.1-10.1 0.3-20.1 0.6-30.2 0.5-5.4 0-10.7-0.1-15.9 0.6-2.3 0.3-4-1.3-6.5-0.6 0.2 0.4 0.5 0.9 0.6 1.5-1.9 0-4 0.4-4.9-0.1-4.2-2.2-9.4-1.5-14.1-2.3-1.7-0.3-3.7-0.1-4.3-1.5-0.5-1.3 1.9-1.5 2.6-2.6-4.2-1.4-4.6-5-8.5-7.2-1.5 0.2-0.9 2.8-4.2 1.3 0.3 2.4 4.5 3.9 2.8 6.4-2.3 0.3-3.2-0.8-4.2-1.7-2.5-4-5.1-8.4-5.1-12.7 0.2-6.8 0.2-13.8 3.6-20.4 0.3-0.5 0.3-1 0.8-1.4 0.9-0.9 1.2-2.4 3.6-2.1 2.2 0.2 2.5 1.5 2.6 2.6 0.2 1.4 1.5 1.8 3.2 2.5 0.9-1.4 0.5-2.9 2.6-3.7 0.2-0.1 0.3-0.4 0.3-0.4-3.1-2.2 1.2-2.2 2.3-3.3-3.1-1.8-4-4.3-3.7-7-1.5-0.3-3.1-0.4-4.5 0-1.7 0.6-2.2-0.5-2.9-1 0.6-0.5 0.8-1.1 2.2-1.3 7.6-0.9 15.2-1.7 22.9-2 20-0.7 39.9-0.9 59.9-1 11.9-0.1 23.8 0.4 35.6 1.1 3.6 0.2 7.1-0.9 10.7-0.5 7.9 0.9 15.8 0.3 23.8 0.5 7.3 0.1 14.4-0.6 21.7-0.1 12.2 0.9 24.4 0.3 36.7 0.6 9.4 0.3 18.9 0.4 28.2 1 11.9 0.7 23.8 1.3 35.6 2 11.1 0.6 22.4 0.5 33.3 2 7.1 1 14.4 1.1 21.3 2.4 4 0.7 8.2 1.6 12.4 1.9 2.2 0.2 0.9 1 1.5 1.5-4-0.8-8-0.8-12.1-1.4-4.3-0.7-8.5-1-12.8 0.4-2.9 1-6.3 0.2-9.3-0.1-10.2-1.1-20.6-1.6-30.8-2.4-12.1-0.9-24.3-1.4-36.4-2.1-9.9-0.6-20-0.5-29.9-1-11.4-0.6-22.7 0-34.2-0.5-6.3-0.3-12.3-0.3-18.5-0.4-4.2-0.1-8.4 1.3-12.8 0.3 0.6 0.2 1.2 0.7 1.9 0.7 10.5 0 20.9 1.9 31.6 1.7 6.5-0.1 13.1 0.2 19.8 0.8 3.2 0.3 6.3-0.4 9.7-0.1 7.6 0.7 15.5 0.5 23 0.8 12.4 0.5 24.7 0.4 37.1 1.1 13.3 0.7 26.8 2.1 39.9 4.1 6.2 0.9 12.7 1.5 19.2 1.7 0.6 0 1.1 0.1 1.5 0.5-4.6 0.1-9.3 0-13.9-0.5-0.6 1.1 1.4 0.9 1.5 1.9-9.7 1.6-19.6-1.4-29.4-0.1 2.2 1.4 5.1 1 7.4 1 7.3 0.1 14.1 1.3 21.2 1.9 2.8 0.3 5.9 0 8.5 0.8 1.5 0.5 4.6-1.1 4.9 1.3 4-0.7 7.3 1.5 11.1 1.2 4-0.3 7.7 0.6 11.6 1.1 0.8 0.1 2.2 0.3 2.3 1.1 0.2 1-1.1 1.2-2 1.5-3.4 1-6.7-0.4-10.1-0.4-0.9 0-2-0.2-2.9-0.2-9.4 0.1-18.8-1.3-28.3-1.8-6-0.4-12.1-0.9-18.1-1.3 0 0.2 0 0.4-0.2 0.6 6.1 0.5 12.1 1.4 18.3 0.7z"></path>
                 </svg>
              </span>
             </span>
             </h1>
             <p className="mb-0 text-gray-600">Marché de l’apprentissage et de l’enseignement en ligne avec 5K + cours et 10 millions d’étudiants. Enseigné par des experts pour vous aider à acquérir de nouvelles compétences.</p>
+            <div className='flex w-full gap-2 flex-wrap'>
+                <span className='flex items-center text-sm gap-1 text-gray-500'><CloudeCheck width={15} height={15} color={'black'}/> Apprentissage professionnel</span>
+                <span className='flex items-center text-sm gap-1 text-gray-500'><CloudeCheck width={15} height={15} color={'black'}/> Apprentissage professionnel</span>
+                <span className='flex items-center text-sm gap-1 text-gray-500'><CloudeCheck width={15} height={15} color={'black'}/> Apprentissage professionnel</span>
+            </div> 
+            <div className='w-full flex justify-start'>
+                <button className='bg-red-100 text-whtie font-medium rounded px-3 py-2.5 text-red-500 hover:text-white hover:bg-red-500'>Commencer</button>
+            </div>
         </div>
-        <div className='w-1/2 flex items-center justify-center'>
-            <div className='rounded-full w-[600px] h-[600px] flex justify-center items-end overflow-hidden' style={{backgroundColor : '#1D3B53'}}>
-                <Image src={'/photo/couv.png'} width={450} height={450} alt='couv' className='mb-2.5'/>
+        <div className='w-full lg:w-1/2 items-center justify-center flex relative'>
+            <div className='glass rounded-xl px-2.5 py-2 flex items-center gap-2'>
+                <div className='p-1 p-sm-3 bg-yellow-500 rounded-full'>
+                    <Mail width={25} height={25} color={'white'}/>
+                </div>
+                <div >
+                    <h6 className='mb-0 font-semibold text-white text-sm sm:text-lg'>Félécitation</h6>
+                    <p  className='mb-0 text-white text-xs  sm:text-sm md:text-base'>Votre addmission est validé</p>
+                </div>
+            </div>
+            <div className='glass rounded-xl px-2.5 py-2 flex flex-col items-center gap-2'>
+                <h6 className='text-white font-semibold text-sm sm:text-base'>Nos etudiants +100</h6>
+                <div className='flex'>
+                    <img src="https://isomorphic-furyroad.s3.amazonaws.com/public/avatars/avatar-11.webp" alt="John Doe" title="John Doe" draggable="false" loading="lazy" width="40" height="40" class="rizzui-avatar-img inline-flex items-center justify-center flex-shrink-0 rounded-full object-cover !h-9 w-9 sm:!h-10 sm:!w-10" style={{width: "40px", height: "40px",backgroundColor: 'rgb(171, 70, 210)'}}/>
+                    <img src="https://isomorphic-furyroad.s3.amazonaws.com/public/avatars/avatar-11.webp" alt="John Doe" title="John Doe" draggable="false" loading="lazy" width="40" height="40" class="rizzui-avatar-img inline-flex items-center justify-center flex-shrink-0 rounded-full object-cover !h-9 w-9 sm:!h-10 sm:!w-10" style={{width: "40px", height: "40px",backgroundColor: 'rgb(171, 70, 210)'}}/>
+                    <img src="https://isomorphic-furyroad.s3.amazonaws.com/public/avatars/avatar-11.webp" alt="John Doe" title="John Doe" draggable="false" loading="lazy" width="40" height="40" class="rizzui-avatar-img inline-flex items-center justify-center flex-shrink-0 rounded-full object-cover !h-9 w-9 sm:!h-10 sm:!w-10" style={{width: "40px", height: "40px",backgroundColor: 'rgb(171, 70, 210)'}}/>
+                </div>
+            </div>
+            
+            <div className='circle rounded-full flex justify-center items-end overflow-hidden'>
+                <img 
+                    src={'/photo/couv.png'} 
+                    alt='couv' 
+                    className='mb-2.5' 
+                    />
+                
             </div>
         </div>
     </section>
