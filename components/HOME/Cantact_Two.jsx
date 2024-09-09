b12fc66452de510af89143fbac08eb75620a6f78
yahia <yahia.slimani@univ-constantine2.dz>
2024-09-09 22:46:16 +0100

    contact us

--- components/HOME/Cantact_Two.jsx
+++ components/HOME/Cantact_Two.jsx
@@ -20,7 +20,7 @@ export default function Cantact_Two() {
             <label htmlFor="" className="mb-1.5 text-sm font-medium">Message*</label>
             <textarea name="" id=""  className="border rounded-md h-[90px] px-3 py-2" placeholder="Entrez le message que vaille l'envoyer "></textarea>
         </div>
-        <button className="bg-gray-900 font-medium rounded w-full mt-4 text-white px-2 py-2">Envoyez</button>
+        <button className="bg-gray-900 font-medium rounded w-full mt-4 text-white px-2 py-2">Envoyer</button>
       </form>
   )
 }
